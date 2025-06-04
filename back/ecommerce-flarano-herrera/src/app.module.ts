48ba38190365ccfed46554b826d427c3ff1f3bf6
FLAranoHerrera <aranoherrera92@gmail.com>
2025-06-04 10:36:01 -0500

    Middleware global

--- back/ecommerce-flarano-herrera/src/app.module.ts
+++ back/ecommerce-flarano-herrera/src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
   imports: [ProductsModule, UsersModule, AuthModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule { 
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+ }
