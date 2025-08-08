e4950dc9f722df493f2d8e1e97197540fd2960ac
FLAranoHerrera <aranoherrera92@gmail.com>
2025-08-08 00:39:44 -0600

    Deploy

--- back/ecommerce-flarano-herrera/src/config/typeorm.config.ts
+++ back/ecommerce-flarano-herrera/src/config/typeorm.config.ts
@@ -9,16 +9,35 @@ import { OrderDetail } from '../entities/orderDetail.entity';
 export const typeOrmConfigAsync = {
   imports: [ConfigModule],
   inject: [ConfigService],
-  useFactory: async (configService: ConfigService): Promise<TypeOrmModuleOptions> => ({
-    type: 'postgres',
-    host: configService.get<string>('DB_HOST'),
-    port: parseInt(configService.get<string>('DB_PORT')!, 10),
-    username: configService.get<string>('DB_USERNAME'),
-    password: String(configService.get('DB_PASSWORD') ?? ''),
-    database: configService.get<string>('DB_NAME'),
-    dropSchema: false,
-    entities: [User, Product, Category, Order, OrderDetail],
-    synchronize: true,
-    logging: true,
-  }),
+  useFactory: async (
+    configService: ConfigService,
+  ): Promise<TypeOrmModuleOptions> => {
+    // La lógica para decidir qué configuración usar
+    if (configService.get<string>('DATABASE_URL')) {
+      // Opción 1: Usar DATABASE_URL si existe (para producción en Render)
+      return {
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        ssl: {
+          rejectUnauthorized: false, // Requerido por Render
+        },
+        entities: [User, Product, Category, Order, OrderDetail],
+        synchronize: false, // Considera ponerlo en false en producción a futuro
+        logging: true,
+      };
+    } else {
+      // Opción 2: Usar variables individuales si no hay DATABASE_URL (para desarrollo local)
+      return {
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: parseInt(configService.get<string>('DB_PORT')!, 10),
+        username: configService.get<string>('DB_USERNAME'),
+        password: String(configService.get('DB_PASSWORD') ?? ''),
+        database: configService.get<string>('DB_NAME'),
+        entities: [User, Product, Category, Order, OrderDetail],
+        synchronize: true,
+        logging: true,
+      };
+    }
+  },
 };
