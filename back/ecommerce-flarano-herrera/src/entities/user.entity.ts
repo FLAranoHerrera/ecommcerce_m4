6ad6f01de9d2f3d23baddb08e3a06e25a17a8be7
FLAranoHerrera <aranoherrera92@gmail.com>
2025-09-15 13:23:01 -0500

    Entidad y DTO de phone

--- back/ecommerce-flarano-herrera/src/entities/user.entity.ts
+++ back/ecommerce-flarano-herrera/src/entities/user.entity.ts
@@ -23,8 +23,8 @@ export class User {
   @Column({ length: 100 })
   password: string;
 
-  @Column({ type: 'bigint', nullable: true })
-  phone: number;
+  @Column({ type: 'varchar', nullable: true })
+  phone: string;
 
   @Column({ type: 'varchar', length: 50, nullable: true })
   country: string;
