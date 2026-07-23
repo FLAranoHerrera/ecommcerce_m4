cbad3c4bd3b3815c7353e63818ec8de09d482dfb
Francisco Arano <aranoherrera92@gmail.com>
2026-07-22 22:20:23 -0600

    fix Swagger pagination parameter schemas

--- src/modules/orders/dto/order-query.dto.ts
+++ src/modules/orders/dto/order-query.dto.ts
@@ -3,14 +3,19 @@ import { IsInt, IsOptional, Max, Min } from 'class-validator';
 import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class OrderQueryDto {
-  @ApiPropertyOptional({ default: 1, minimum: 1 })
+  @ApiPropertyOptional({ type: Number, default: 1, minimum: 1 })
   @Type(() => Number)
   @IsInt()
   @Min(1)
   @IsOptional()
   page = 1;
 
-  @ApiPropertyOptional({ default: 10, minimum: 1, maximum: 100 })
+  @ApiPropertyOptional({
+    type: Number,
+    default: 10,
+    minimum: 1,
+    maximum: 100,
+  })
   @Type(() => Number)
   @IsInt()
   @Min(1)
